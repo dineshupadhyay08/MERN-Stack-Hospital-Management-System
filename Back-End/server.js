34fd70a2228f97666619c1ca29f4239e17a82d33
dineshupadhyay08 <dineshupadhyay666.com>
2025-11-29 23:53:40 +0530

    First commit

--- Back-End/server.js
+++ Back-End/server.js
@@ -2,14 +2,22 @@ import app from "./app.js";
 import cloudinary from "cloudinary";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 
+// Cloudinary Config
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+// Default Root Route (Fix for Cannot GET /)
+app.get("/", (req, res) => {
+  res.send("Hospital Management Backend API is Running...");
+});
+
+// Error Middleware
 app.use(errorMiddleware);
 
+// Start Server
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
