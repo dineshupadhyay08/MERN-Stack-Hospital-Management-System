2a4434590ee17d55f32934cf1798a454af094f37
dineshupadhyay08 <dineshupadhyay666.com>
2025-12-02 18:38:35 +0530

    CORS fixed for dashboard

--- Back-End/utils/jwtToken.js
+++ Back-End/utils/jwtToken.js
@@ -1,12 +1,15 @@
 export const generateToken = (user, message, statusCode, res) => {
   const token = user.generateJsonWebToken();
+
   const cookieName = user.role === "Admin" ? "adminToken" : "patientToken";
+
   res
     .status(statusCode)
     .cookie(cookieName, token, {
-      expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
+      httpOnly: true, // Prevent JS access
+      secure: true, // Required for HTTPS (Render + Vercel)
+      sameSite: "none", // Required for cross-site cookies
+      maxAge: process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000,
     })
     .json({
       success: true,
